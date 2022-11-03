d9783c192d673d99386772597772cf62b3b0a959
Flávia Poliane Fonseca <104723133+FlaviaPFonseca@users.noreply.github.com>
2022-11-03 08:50:19 -0300

    arquivo 2

---
+++ src/app/Pages/office.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-office',
+  templateUrl: './office.component.html',
+  styleUrls: ['./office.component.css']
+})
+export class OfficeComponent implements OnInit {
+
+  constructor() { /* TODO document why this constructor is empty */  }
+
+  ngOnInit(): void {
+    // TODO document why this method 'ngOnInit' is empty
+
+  }
+
+}
